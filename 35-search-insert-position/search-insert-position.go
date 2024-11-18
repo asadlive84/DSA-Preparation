760a94e5ce491e5a685a7c1505461d4f744c390f
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2024-11-19 00:12:22 +0600

    Time: 0 ms (100.00%) | Memory: 4.5 MB (45.53%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.go
@@ -0,0 +1,16 @@
+func searchInsert(nums []int, target int) int {
+
+	for i := 0; i < len(nums); i++ {
+
+		if nums[i] == target {
+			return i
+		}
+
+		if nums[i] > target {
+			return i
+		}
+
+	}
+
+	return len(nums)
+}
