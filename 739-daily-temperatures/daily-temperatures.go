bf069518d18bbabda2352a8a22c832dbb1e003e3
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2024-12-05 20:09:42 +0600

    Time: 9 ms (78.44%) | Memory: 10.2 MB (82.08%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.go
@@ -0,0 +1,58 @@
+func dailyTemperatures(temperatures []int) []int {
+
+	stack := []int{}
+	result := make([]int, len(temperatures))
+
+	for i := len(temperatures) - 1; i >= 0; i-- {
+		//fmt.Println("stack::", stack, " ", " i ", i, " ", temperatures[i], " result ", result)
+		if len(stack) == 0 {
+			stack = append(stack, i)
+			result[i] = 0
+			continue
+		}
+
+		if temperatures[i] == temperatures[stack[len(stack)-1]] {
+
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				stack = append(stack, i)
+				result[i] = 0
+				continue
+			}
+			result[i] = stack[len(stack)-1] - i
+			stack = append(stack, i)
+			continue
+		}
+
+		for len(stack) != 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
+			stack = stack[:len(stack)-1]
+		}
+
+		if len(stack) == 0 {
+			stack = append(stack, i)
+			result[i] = 0
+			continue
+		}
+
+		if temperatures[i] == temperatures[stack[len(stack)-1]] {
+
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				stack = append(stack, i)
+				result[i] = 0
+				continue
+			}
+			result[i] = stack[len(stack)-1] - i
+			stack = append(stack, i)
+			continue
+		}
+
+		if temperatures[i] < temperatures[stack[len(stack)-1]] {
+			result[i] = stack[len(stack)-1] - i
+			stack = append(stack, i)
+		}
+
+	}
+
+	return result
+}
