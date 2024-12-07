452a0550dc681d86c24adb622cbd24f1dc15523f
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2024-12-07 16:31:43 +0600

    Time: 2404 ms (8.14%) | Memory: 65.8 MB (19.80%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
@@ -3,12 +3,12 @@ func minRemoveToMakeValid(s string) string {
 	index := make([]bool, len(s))
 
 	for i := 0; i < len(s); i++ {
-		if string(s[i]) == "(" {
+		if s[i] == '(' {
 			stack = append(stack, i)
 		} else {
 			if len(stack) == 0 {
 				continue
-			} else if string(s[i]) == ")" && string(s[stack[len(stack)-1]]) == "(" {
+			} else if s[i] == ')' && s[stack[len(stack)-1]] == '(' {
 				index[i] = true
 				index[stack[len(stack)-1]] = true
 				stack = stack[:len(stack)-1]
@@ -18,7 +18,7 @@ func minRemoveToMakeValid(s string) string {
 
 	newStr := ""
 	for i := 0; i < len(s); i++ {
-		if string(s[i]) == "(" || string(s[i]) == ")" {
+		if s[i] == '(' || s[i] == ')' {
 			if index[i] {
 				newStr += string(s[i])
 			}
