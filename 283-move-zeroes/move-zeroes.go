f9b549552ab8678ffdedd6b2e97d3457b7fac232
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2024-12-11 23:35:51 +0600

    Time: 0 ms (100.00%) | Memory: 9.3 MB (13.00%) - LeetSync

--- 283-move-zeroes/move-zeroes.go
+++ 283-move-zeroes/move-zeroes.go
@@ -1,18 +1,17 @@
 func moveZeroes(nums []int) {
 
-	newNums := make([]int, len(nums), len(nums))
 	i := 0
 	for _, data := range nums {
-
 		if data != 0 {
-			newNums[i] = data
+			nums[i] = data
 			i++
 		}
 
 	}
 
-	for index, data := range newNums {
-		nums[index] = data
+	for i <= len(nums)-1 {
+		nums[i] = 0
+		i++
 	}
 
 }
