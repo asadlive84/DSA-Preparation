d44383ef162537cfc7e1550744e3155a6e681496
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2024-12-24 02:19:13 +0600

    Time: 0 ms (100.00%) | Memory: 4.2 MB (26.08%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+
+	if root == nil {
+		return nil
+	}
+
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+
+		
+			curr.Left, curr.Right = curr.Right, curr.Left
+		
+
+		if curr.Left != nil {
+			queue = append(queue, curr.Left)
+		}
+		if curr.Right != nil {
+			queue = append(queue, curr.Right)
+		}
+
+	}
+
+    return root
+}
