af80334b42799349072ed0ab4bafcd224786891f
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2025-01-02 13:55:24 +0600

    Time: 0 ms (100.00%) | Memory: 5.9 MB (10.42%) - LeetSync

--- 733-flood-fill/flood-fill.go
+++ 733-flood-fill/flood-fill.go
@@ -1,38 +1,28 @@
 func floodFill(image [][]int, sr int, sc int, color int) [][]int {
-
-	orginalColor := image[sr][sc]
-	if orginalColor == color {
-		return image
+	originalColor := image[sr][sc]
+	if originalColor == color {
+		return image // No need to fill if the target color is the same as the original.
 	}
 
-	fill := func(image [][]int, row, col, color, orginalColor int) {
-		stack := [][]int{{row, col}}
+	rows, cols := len(image), len(image[0])
+	stack := [][]int{{sr, sc}} // Start with the initial position.
 
-		for len(stack) > 0 {
-			curr := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
+	for len(stack) > 0 {
+		// Pop the last element from the stack.
+		curr := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
 
-			r, c := curr[0], curr[1]
+		r, c := curr[0], curr[1]
 
-			if r < 0 || c < 0 || r >= len(image) || c >= len(image[0]) || image[r][c] == color {
-				continue
-			}
-			if image[r][c] != orginalColor {
-				continue
-
-			}
+		// Ensure the current cell is within bounds and matches the original color.
+		if r >= 0 && r < rows && c >= 0 && c < cols && image[r][c] == originalColor {
+			// Update the cell with the new color.
 			image[r][c] = color
 
-			stack = append(stack, []int{r + 1, c})
-			stack = append(stack, []int{r - 1, c})
-			stack = append(stack, []int{r, c + 1})
-			stack = append(stack, []int{r, c - 1})
-
+			// Add neighboring cells to the stack.
+			stack = append(stack, []int{r + 1, c}, []int{r - 1, c}, []int{r, c + 1}, []int{r, c - 1})
 		}
-
 	}
-
-	fill(image, sr, sc, color, orginalColor)
 
 	return image
 }
