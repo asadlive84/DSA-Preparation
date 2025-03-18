411e3e802e4baf20cb6bc2f9fca4467b153291cf
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2025-03-18 23:02:46 +0600

    Time: 20 ms (28.11%) | Memory: 12.2 MB (44.61%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.go
+++ 739-daily-temperatures/daily-temperatures.go
@@ -1,40 +1,15 @@
 func dailyTemperatures(temperatures []int) []int {
-
+	record := make([]int, len(temperatures))
 	stack := []int{}
-	result := make([]int, len(temperatures))
 
-	for i := len(temperatures) - 1; i >= 0; i-- {
-		//fmt.Println("stack::", stack, " ", " i ", i, " ", temperatures[i], " result ", result)
-
+	for i := range temperatures {
 		for len(stack) > 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
+			prev := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
+			record[prev] = i - prev
+
 		}
-
-		if len(stack) == 0 {
-			stack = append(stack, i)
-			result[i] = 0
-			continue
-		}
-
-		if temperatures[i] == temperatures[stack[len(stack)-1]] {
-
-			stack = stack[:len(stack)-1]
-			if len(stack) == 0 {
-				stack = append(stack, i)
-				result[i] = 0
-				continue
-			}
-			result[i] = stack[len(stack)-1] - i
-			stack = append(stack, i)
-			continue
-		}
-
-		if temperatures[i] < temperatures[stack[len(stack)-1]] {
-			result[i] = stack[len(stack)-1] - i
-			stack = append(stack, i)
-		}
-
+		stack = append(stack, i)
 	}
-
-	return result
+	return record
 }
