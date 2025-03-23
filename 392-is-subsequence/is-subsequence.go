e20c18e56174b65ff31dd5da39fd05fb53a86f4e
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2025-03-24 02:45:06 +0600

    Time: 0 ms (100.00%) | Memory: 4 MB (73.33%) - LeetSync

--- 392-is-subsequence/is-subsequence.go
+++ 392-is-subsequence/is-subsequence.go
@@ -1,19 +1,17 @@
-
 func isSubsequence(s string, t string) bool {
-
 	left, right := 0, 0
 
-	for right < len(t) {
-        if left == len(s){
-            return true
-        }
-		if s[left] == t[right] {
+	for right <= len(t) {
+		if left == len(s) {
+			return true
+		}
+		
+		if right <len(t) && s[left] == t[right] {
 			left++
 
 		}
 		right++
-
 	}
 
-	return left == len(s)
+	return false
 }
