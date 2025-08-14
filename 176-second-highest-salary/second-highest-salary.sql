0b66e84590892137efb95ae62b0877883f647119
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2025-08-14 19:36:38 +0600

    Time: 368 ms (10.93%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,10 @@
+SELECT MAX(SecondHighestSalary) AS SecondHighestSalary
+FROM (
+    SELECT salary AS SecondHighestSalary
+    FROM (
+        SELECT salary,
+               DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+        FROM Employee
+    ) ranked
+    WHERE rnk = 2
+) t;
